fe95684e08bdbc05ec7ba3ca9f11df5a371dfd58
Shruti Jain <jainshruti0512@gmail.com>
2023-11-18 10:02:19 +0530

    portfolio

---
+++ src/data/projects.js
@@ -0,0 +1,31 @@
+export default function getProjects() {
+    return [{
+
+            id: 1,
+            image: 'bewakoof.png',
+            projectTitle: "Bewakoog Clone",
+            projectInfo: "clone of bewakoof ecommerce",
+            techUsed: ["JavaScript", "HTML", "CSS"],
+            projectLinkHosted: "https://647dee0e37f98815af0aa4fe--glittery-pika-ebf20b.netlify.app/",
+            projectLinkGithub: "https://github.com/Shrutij512/bewakoof_clone",
+        },
+        {
+            id: 2,
+            projectTitle: "Mine Wine",
+            image: 'Mine-wine.png',
+            projectInfo: "It's a clone of Mine-Wine app created using HTML, CSS, Javascript",
+            techUsed: ["HTML", "CSS", "Javascript"],
+            projectLinkHosted: "",
+            projectLinkGithub: "https://github.com/Shrutij512/Wine_shop-Project",
+        },
+        {
+            id: 3,
+            projectTitle: "Carver Skateboards",
+            image: 'Mine-wine.png',
+            projectInfo: "About Experience urban wave riding with our high-quality skateboards. This repository features a user-friendly ecommerce website with Razorpay integration for seamless payments.",
+            techUsed: ["React", "JSON-server"],
+            projectLinkHosted: "https://650c065e34b6d7008525689c--effulgent-granita-28ab37.netlify.app/",
+            projectLinkGithub: "https://github.com/anasrashid-1/carverskateboards",
+        }
+    ];
+}
